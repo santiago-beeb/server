cdaacc66d8145a3d282909db9d2f6cef7533f7d4
Santiago Alzate <salzategarcia@gmail.com>
2023-10-09 15:33:46 -0500

    ADD productforwomen

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -2,20 +2,42 @@ import { getConnection } from "../database/database.js";
 
 const getProducts = async (req, res) => {
   try {
-    // Obtiene una conexión a la base de datos
     const connection = await getConnection();
-
-    // Consulta todos los productos en la base de datos
-    const [products] = await connection.query("SELECT * FROM producto");
-
-    // Envía la lista de productos en la respuesta
+    const [products] = await connection.query(
+      "SELECT * FROM producto WHERE pdc_estado = 1"
+    );
     res.status(200).json(products);
   } catch (error) {
-    // En caso de error, envía una respuesta de error
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const getProductsForMen = async (req, res) => {
+  try {
+    const connection = await getConnection();
+    const [productsForMen] = await connection.query(
+      "SELECT * FROM producto WHERE pdc_fk_seccion = 1 AND pdc_estado = 1"
+    );
+    res.status(200).json(productsForMen);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const getProductsForWomen = async (req, res) => {
+  try {
+    const connection = await getConnection();
+    const [productsForWomen] = await connection.query(
+      "SELECT * FROM producto WHERE pdc_fk_seccion = 2 AND pdc_estado = 1"
+    );
+    res.status(200).json(productsForWomen);
+  } catch (error) {
     res.status(500).json({ error: error.message });
   }
 };
 
 export const methods = {
-    getProducts
+  getProducts,
+  getProductsForMen,
+  getProductsForWomen,
 };
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,9 +1,17 @@
+// En tu archivo de rutas
 import { Router } from "express";
 import { methods as productController } from "../controllers/product.controller.js";
 import { checkAdminRole, requireAuth } from "../middleware/auth.handler.js";
 
 const router = Router();
 
-router.get("/get-products", productController.getProducts);
+// Ruta para obtener todos los productos
+router.get("/products", productController.getProducts);
+
+// Nueva ruta para obtener productos para hombres
+router.get("/products-for-men", productController.getProductsForMen);
+
+// Nueva ruta para obtener productos para mujeres
+router.get("/products-for-women", productController.getProductsForWomen);
 
 export default router;
