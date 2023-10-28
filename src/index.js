3a65ef845076cfcbc6b79aadbd6c17a398ae5f0a
Santiago Alzate <salzategarcia@gmail.com>
2023-10-28 15:06:34 -0500

    ADD new-credentials

--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
+import { app } from "./app";
 
-//Routes
-import userRoutes from "./routes/user.routes.js";
-import productRoutes from "./routes/product.routes.js";
+const main = () => {
+  app.listen(app.get("port"));
+  console.log(`puerto ${app.get("port")}`);
+};
 
-const app = express();
-
-//settings
-const PORT = process.env.PORT || 3001;
-app.listen(PORT);
-
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(cors());
-
-//Route
-app.use("/api/user", userRoutes);
-app.use("/api/product", productRoutes);
-
-export { app };
+main();
+a
