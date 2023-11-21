28b1ea96b556902565c2c2e449e4c9aaf6aabb00
Santiago Alzate <salzategarcia@gmail.com>
2023-11-20 19:55:34 -0500

    FIX db

--- src/database/database.js
+++ src/database/database.js
@@ -1,10 +1,12 @@
 import Sequelize from "sequelize";
+import mysql2 from "mysql2";
 import config from "../config.js";
 
 const sequelize = new Sequelize(config.database, config.user, config.password, {
   host: config.host,
   port: config.port,
   dialect: "mysql",
+  dialectModule: mysql2,
 });
 
 export const getConnection = () => sequelize;
