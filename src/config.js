d251f7e506ddfe2ae7d3902ec2ef31370d685634
Santiago Alzate <salzategarcia@gmail.com>
2023-12-01 22:32:54 -0500

    FIX Environment-Variables

--- src/config.js
+++ src/config.js
@@ -1,14 +1,12 @@
 import { config } from "dotenv";
 
-config(); //habilita variables de entorno declaradas en el archivo .env
+config(); 
 
-//variables dentro de .env para no compartirlas
 export default {
   host: process.env.HOST || "",
   database: process.env.DATABASE || "",
   user: process.env.USER || "",
   password: process.env.PASSWORD || "",
   port: process.env.DB_PORT || "",
-  key: process.env.SECRET_KEY,
-  passemail: process.env.PASSEMAIL,
+  key: process.env.SECRET_KEY
 };
