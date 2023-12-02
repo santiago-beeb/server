1c7f9b78f0be086e027c3aeadae69e687d5f0a90
Santiago Alzate <salzategarcia@gmail.com>
2023-12-01 22:50:43 -0500

    FIX Environment-Variables

--- src/database/database.js
+++ src/database/database.js
@@ -2,11 +2,16 @@ import Sequelize from "sequelize";
 import mysql2 from "mysql2";
 import config from "../config.js";
 
-const sequelize = new Sequelize(config.database, config.user, config.password, {
-  host: config.host,
-  port: config.port,
-  dialect: "mysql",
-  dialectModule: mysql2,
-});
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.USER,
+  process.env.PASSWORD,
+  {
+    host: process.env.HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    dialectModule: mysql2,
+  }
+);
 
 export const getConnection = () => sequelize;
